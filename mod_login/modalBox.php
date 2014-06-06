447c0e5b67f138d28a4d5bbf31723e7d8b45fee5
Sven Schultschik <sven@schultschik.de>
2014-06-06 14:06:28 +0200

    # stop loading joomla core bootstrap

--- mod_login/modalBox.php
+++ mod_login/modalBox.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 require_once JPATH_SITE . '/components/com_users/helpers/route.php';
 
 JHtml::_('behavior.keepalive');
-JHtml::_('bootstrap.tooltip');
 
 ?>
 <!-- Button trigger modal -->
